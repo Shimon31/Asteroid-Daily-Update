d6d9904976f7cd3d062f29f7b8bf84c8d69a14f5
Shimon31 <shimonbhuiya02@gmail.com>
2024-11-11 17:11:08 +0600

    Daily Image of the day-4

--- app/src/main/java/com/shimon/nasaasteroidpictureupdate/utils/NetworkUtils.kt
+++ app/src/main/java/com/shimon/nasaasteroidpictureupdate/utils/NetworkUtils.kt
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-fun parseAsteroidJsonResult(jsonObject: JSONObject): ArrayList<Asteroid>? {
+fun parseAsteroidJsonResult(jsonObject: JSONObject): ArrayList<Asteroid> {
 
     val nearEarthObjects = jsonObject.getJSONObject("near_earth_objects")
 
@@ -23,18 +23,38 @@ fun parseAsteroidJsonResult(jsonObject: JSONObject): ArrayList<Asteroid>? {
             for (index in 0 until dateWiseData.length()) {
 
                 val asteroidJson = dateWiseData.getJSONObject(index)
-                val id = asteroidJson.getJSONObject("id")
-                val codeName = asteroidJson.getJSONObject("name")
+                val id = asteroidJson.getLong("id")
+                val codeName = asteroidJson.getString("name")
                 val closeApproachDate = asteroidJson
                     .getJSONArray("close_approach_data")
                     .getJSONObject(0).getString("close_approach_date")
+
+                val isPotentiallyHazardous =
+                    asteroidJson.getBoolean("is_potentially_hazardous_asteroid")
+
+  
+                val asteroid = Asteroid(
+
+                    id = id,
+                    codeName = codeName,
+                    closeApproachDate = closeApproachDate,
+                    absoluteMagnitude = 0.0,
+                    estimatedDiameter = 0.0,
+                    relativeVelocity = 0.0,
+                    distanceFromEarth = 0.0,
+                    isPotentiallyHazardous = isPotentiallyHazardous
+
+
+                )
+
+                asteroidList.add(asteroid)
 
             }
 
         }
 
     }
-    return null
+    return asteroidList
 
 }
 
